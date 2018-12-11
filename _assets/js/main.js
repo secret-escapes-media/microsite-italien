6183756c78bff22dca305d34fe474859219b3d47
Birgit <birgit.voslamber@secretescapes.com>
2018-12-11 10:37:38 +0100

    insidertipps structure added

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,12 +1,12 @@
 // general js for the project that wouldn't be a reuseable component
 
 
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("bv-collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("bv-active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
@@ -15,3 +15,30 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+/*
+ document.addEventListener("DOMContentLoaded", function(event) {
+
+var acc = document.getElementsByClassName("accordion");
+var panel = document.getElementsByClassName('panel');
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        var setClasses = !this.classList.contains('active');
+        setClass(acc, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
+}
+
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
+}
+
+});*/
