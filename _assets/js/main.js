57d281a10efb6b733cfb9c36edaefd089dde6c25
Birgit <birgit.voslamber@secretescapes.com>
2018-12-12 15:14:56 +0100

    insidertipps banner included

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,44 +1,22 @@
 // general js for the project that wouldn't be a reuseable component
 
+function toggle(){}
 
 var coll = document.getElementsByClassName("bv-collapsible");
+var collb = document.getElementsByClassName("bv-collapsible2");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("bv-active");
-    var content = this.nextElementSibling;
+    var content = this.previousElementSibling;
+    //alert(content);
     if (content.style.maxHeight){
       content.style.maxHeight = null;
+
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+
     }
   });
 }
-
-/*
- document.addEventListener("DOMContentLoaded", function(event) {
-
-var acc = document.getElementsByClassName("accordion");
-var panel = document.getElementsByClassName('panel');
-
-for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function() {
-        var setClasses = !this.classList.contains('active');
-        setClass(acc, 'active', 'remove');
-        setClass(panel, 'show', 'remove');
-
-        if (setClasses) {
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        }
-    }
-}
-
-function setClass(els, className, fnName) {
-    for (var i = 0; i < els.length; i++) {
-        els[i].classList[fnName](className);
-    }
-}
-
-});*/
